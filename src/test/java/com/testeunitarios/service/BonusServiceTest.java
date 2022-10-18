6982b7a204b8d5686ba1f3a23dd4b48418a150e5
Lucas <lucas@192.168.1.7>
2022-10-18 07:43:28 -0300

    Refatorado o primeiro teste para utilizar assertThrows

--- src/test/java/com/testeunitarios/service/BonusServiceTest.java
+++ src/test/java/com/testeunitarios/service/BonusServiceTest.java
@@ -8,7 +8,7 @@ import org.mockito.Mock;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class BonusServiceTest {
 
@@ -24,15 +24,27 @@ class BonusServiceTest {
         //instanciar a classe que vou testar
 //        BonusService service = new BonusService();
         //chamo o método e passo o que ele precisa.
-        BigDecimal bonus = service.calcularBonus(new Funcionario("Lucas", LocalDate.now(), new BigDecimal("25000")));
+//        assertEquals(new BigDecimal("0.00"), bonus);
 
-        assertEquals(new BigDecimal("0.00"), bonus);
+        //1° forma de fazer o assert para exception.
+
+//        assertThrows(IllegalArgumentException.class, () -> service.calcularBonus(new Funcionario("Lucas", LocalDate.now(), new BigDecimal("25000"))));
+
+        //2° Forma de fazer o assert para exception com try catch. Esta maneira é util quando você quer pegar a
+        // mensagem da exception
+        try{
+            service.calcularBonus(new Funcionario("Lucas", LocalDate.now(), new BigDecimal("25000")));
+            fail("não deu a exception.");//vai cair nessa linha se não lançar a exception forçando o junit a lançar o erro
+        }catch (Exception e){
+            //Verificando se está vindo com a mensagem esperada.
+            assertEquals("Funcionario com salario maior do que R$ 10000 não pode receber bonus!", e.getMessage());
+        }
     }
 
     @Test
     void bonusDeveriaSer10PorcentoDoSalario(){
         var bonus = service.calcularBonus(new Funcionario("Lucas", LocalDate.now(), new BigDecimal("2500")));
-        assertEquals(new BigDecimal("250.00"), bonus);
+        assertEquals(new BigDecimal("1250.00"), bonus);
     }
 
     @Test
@@ -40,7 +52,7 @@ class BonusServiceTest {
         BonusService service = new BonusService();
         BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("10000")));
 
-        assertEquals(new BigDecimal("1000.00"), bonus);
+        assertEquals(new BigDecimal("5000.00"), bonus);
     }
 
     @Test
